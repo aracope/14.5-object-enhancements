43cbfcb3755796ba27a17e403e1728a4bd53daeb
aracope <156398622+aracope@users.noreply.github.com>
2025-01-28 17:04:41 -0800

    Complete createAnimal function.

--- script.js
+++ script.js
@@ -64,3 +64,20 @@ const instructor2 = {
 // const s = createAnimal("sheep", "bleet", "BAAAAaaaa")
 // {species: "sheep", bleet: ƒ}
 // s.bleet() //"BAAAAaaaa"
+
+const d = createAnimal("dog", "bark", "Woooof!")
+// {species: "dog", bark: ƒ}
+d.bark()  //"Woooof!"
+
+const s = createAnimal("sheep", "bleet", "BAAAAaaaa")
+// {species: "sheep", bleet: ƒ}
+s.bleet() //"BAAAAaaaa"
+
+function createAnimal(species, verb, noise) {
+  return {
+    species,
+    [verb]() {
+      return noise;
+    }
+  }
+}
